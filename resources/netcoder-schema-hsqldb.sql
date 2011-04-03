770113bb9627bf2084fa9739c26b4d0249160785
David Hovemeyer <dhovemey@ycp.edu>
2011-04-03 07:04:03 -0400

    added support for hsqldb (untested)
    this gives us an easy pure java deployment option

---
+++ resources/netcoder-schema-hsqldb.sql
@@ -0,0 +1,20 @@
+CREATE TABLE changes (
+  id integer IDENTITY,
+  user_id integer NOT NULL,
+  problem_id integer NOT NULL,
+  type varchar(20) DEFAULT NULL,   -- no enum datatypes in hsqldb
+  start_row integer NOT NULL,
+  end_row integer NOT NULL,
+  start_col integer NOT NULL,
+  end_col integer NOT NULL,
+  timestamp bigint NOT NULL,
+  text longvarchar NOT NULL
+);
+
+CREATE TABLE problems (
+  problem_id integer IDENTITY,
+  testname varchar(255) NOT NULL,
+  description longvarchar NOT NULL
+);
+
+INSERT INTO problems VALUES(1, 'sq', 'Square a number.');
