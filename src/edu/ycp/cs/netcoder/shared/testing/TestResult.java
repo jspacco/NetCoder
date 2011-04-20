4c9d0658f251e7cfe1d7b82c22d729007d0447b3
David Hovemeyer <dhovemey@ycp.edu>
2011-04-20 08:37:37 -0400

    added some copyright notice comments

--- src/edu/ycp/cs/netcoder/shared/testing/TestResult.java
+++ src/edu/ycp/cs/netcoder/shared/testing/TestResult.java
@@ -1,14 +1,25 @@
+// NetCoder - a web-based pedagogical programming environment
+// Copyright (C) 2011, Jaime Spacco <jspacco@knox.edu>
+// Copyright (C) 2011, David H. Hovemeyer <dhovemey@ycp.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package edu.ycp.cs.netcoder.shared.testing;
 
 import java.io.Serializable;
-import java.security.AccessControlException;
-
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
-
-import edu.ycp.cs.netcoder.server.problems.TestCase;
 
 public class TestResult implements Serializable, IsSerializable
 {
